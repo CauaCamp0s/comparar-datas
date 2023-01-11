ed6e117dd40a8ef885b641fab71ea23aae137197
Cauã Campos <116642309+CauaCamp0s@users.noreply.github.com>
2023-01-11 14:21:14 -0300

    Add files via upload

---
+++ bot-emails/app-email-outlook.py
@@ -0,0 +1,35 @@
+import smtplib
+import email.message
+
+#text highlighter variable
+green_highlighter = '\033[1;36;42m'
+
+#crição do email 
+def send_email():
+    body_email = ''''
+    <p>Olá Cauã,</p>
+    <p>Email automatico, tenha um bom dia!</p>
+    '''
+
+#Abrir email e enviar    
+    msg = email.message.Message()
+    msg['Subject'] = 'Teste envio de email com python'
+    msg['From'] = 'caua.ciee@sergipegas.com.br'
+    msg['To'] = 'caua.campos.job@gmail.com'
+    senha = 'ca120400!'
+    
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(body_email)
+    
+    s = smtplib.SMTP('smtp.office365.com:587')
+    s.connect('smtp.office365.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+
+    s.login(msg['From'], senha)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print (f'{green_highlighter}Sent email!')
+    
+send_email()
+
