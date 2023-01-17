47a32eb1120fa06ee7633f5efc567eb7d9980f07
Cauã Campos <116642309+CauaCamp0s@users.noreply.github.com>
2023-01-17 18:24:14 -0300

    Update gmailRedesergas.py

--- bot-emails/gmailRedesergas.py
+++ bot-emails/gmailRedesergas.py
@@ -13,14 +13,14 @@ def SendEmail():
     #Abrir email e enviar    
     msg = email.message.Message()
     msg['Subject'] = 'Teste envio de email com python'
-    msg['From'] = 'cauacampos258@gmail.com'
+    msg['From'] = '##############@gmail.com'
     msg['To'] = 'sendmail@sergipegas.com.br'
     senha = 'akvcqskunyxhkpuz'
     
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(body_email)
     
-    s = smtplib.SMTP('malibu.sergipegas.com.br:25')
+    s = smtplib.SMTP('##########.com.br:25')
     s.connect('malibu', 25)
     s.ehlo()
     s.sendmail (msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
